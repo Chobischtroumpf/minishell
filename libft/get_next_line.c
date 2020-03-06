c6261d7cdecf65b661362b0ba256221f47e622d0
adorigo <adorigo@student.s19.be>
2020-03-06 13:15:57 +0100

    started parsing, I now get the amount of args, and count the insides of " as one arg only

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -3,73 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adorigo <adorigo@student.42.fr>            +#+  +:+       +#+        */
+/*   By: adorigo <adorigo@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:54:16 by adorigo           #+#    #+#             */
-/*   Updated: 2020/01/21 07:48:39 by adorigo          ###   ########.fr       */
+/*   Updated: 2020/03/06 00:39:41 by adorigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char		*ft_strnewone(void)
+int		get_next_line(int fd, char **line)
 {
-	char *ret;
+	static char		buf[FOPEN_MAX][BUFFER_SIZE + 1];
+	int				ret;
+	int				i;
 
-	if (!(ret = (char *)malloc(sizeof(char))))
+	if (BUFFER_SIZE < 1 || fd < 0 || fd >= FOPEN_MAX || !line
+					|| !(*line = ft_strjoin_to_eol(NULL, buf[fd])))
+		return (-1);
+	ret = 1;
+	while (ft_strchr_pos(buf[fd], '\n') == -1 && ret)
+	{
+		ret = read(fd, buf[fd], BUFFER_SIZE);
+		if (ret == -1)
+			return (-1);
+		buf[fd][ret] = '\0';
+		if (!(*line = ft_strjoin_to_eol(*line, buf[fd])))
+			return (-1);
+	}
+	i = 0;
+	ret = ft_strchr_pos(buf[fd], '\n') + 1;
+	if (buf[fd][0] == 0)
 		return (0);
-	ret[0] = '\0';
-	return (ret);
-}
-
-static int		extract(char **line, char **cache, int idx)
-{
-	char	*tmp;
-	int		ret;
-
-	if (idx >= 0)
-	{
-		if (!(*line = ft_substr(*cache, 0, idx)))
-			return (ft_strfree(cache, -1));
-		if (!(tmp = ft_substr(*cache, idx + 1, ft_strlen(*cache) - idx - 1)))
-			return (ft_strfree(cache, -1));
-		ret = 1;
-	}
-	else
-	{
-		if (!(*line = ft_substr(*cache, 0, ft_strlen(*cache))))
-			return (ft_strfree(cache, -1));
-		tmp = 0;
-		ret = 0;
-	}
-	ft_strfree(cache, 0);
-	*cache = tmp;
-	return (ret);
-}
-
-int				get_next_line(int fd, char **line)
-{
-	ssize_t		r_size;
-	char		buff[BUFFER_SIZE + 1];
-	static char	*cache;
-	char		*tmp;
-
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return (ft_strfree(&cache, -1));
-	while ((r_size = read(fd, buff, BUFFER_SIZE)) > 0)
-	{
-		buff[r_size] = '\0';
-		if (!(tmp = ft_strnjoin(cache, buff, r_size)))
-			return (ft_strfree(&cache, -1));
-		ft_strfree(&cache, 0);
-		cache = tmp;
-		if (ft_strnbr(cache) != -1)
-			break ;
-	}
-	if (r_size < 0)
-		return (ft_strfree(&cache, -1));
-	if (r_size == 0 && (!cache || *cache == '\0')
-		&& (*line = ft_strnewone()))
-		return (ft_strfree(&cache, 0));
-	return (extract(line, &cache, ft_strnbr(cache)));
+	while (buf[fd][ret] != '\0' && ret)
+		buf[fd][i++] = buf[fd][ret++];
+	buf[fd][i] = '\0';
+	return (1);
 }
--- minishell.c
+++ minishell.c
@@ -6,12 +6,58 @@
 /*   By: adorigo <adorigo@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 12:54:46 by adorigo           #+#    #+#             */
-/*   Updated: 2020/03/05 19:13:36 by adorigo          ###   ########.fr       */
+/*   Updated: 2020/03/06 12:53:43 by adorigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int			ft_arg_count(char *line)
+{
+	int ck;
+	int cnt;
+
+	cnt = 1;
+	ck = 0;
+	while (*line)
+	{
+		if (!ft_strncmp(line, "\"", 1))
+		{
+			line++;
+			while (ft_strncmp(line, "\"", 1) && *line)
+				line++;
+			if (!line)
+				return (0);
+		}
+		if (*line == ' ' && *line + 1 && ck == 0 && (ck = 1))
+			cnt++;
+		else if (*line == ' ' && ck == 1)
+			continue;
+		else
+			ck = 0;
+		line++;
+	}
+	return(cnt);
+}
+
+// char **ft_arg_split(t_minishell *minishell)
+// {
+
+// 	int word_len;
+// 	char **tab;
+
+// 	word_len = 0;
+// 	//split normal :
+// 	while (*(minishell->line))
+// 	{
+// 		while (minishell->line[word_len] != ' ')
+// 			word_len++;
+// 		if 
+// 	}
+// 	//split if "" or '' :
+
+
+// }
 
 t_minishell	*get_minishell(void)
 {
@@ -35,15 +81,6 @@ char		**get_built_in(void)
 	return (built_in);
 }
 
-// void		init_env_list(char **env)
-// {
-// 	char		**cut;
-// 	int			i;
-// 	t_minishell	*minishell;
-
-// 	minishell = get_minishell();
-
-
 int			ft_exec_pwd(void)
 {
 	char cwd[PATH_MAX];
@@ -65,7 +102,6 @@ static int 	ft_parse_line(char *line)
 {
 	int ret;
 	
-	line = ft_strtrim(line, " \t\n\v\f\r");
 	if (!strncmp(line, "echo", 4))
 		ret = 1;
 		// ret = ft_exec_cd();
@@ -75,6 +111,7 @@ static int 	ft_parse_line(char *line)
 		ret = 0;
 	else
 	{
+		ft_printf("minishell : unknown command : %s\n", line);
 		ret = 1;
 	}
 	return (ret);
@@ -88,8 +125,9 @@ void		signal_handler(int signbr)
 		if (get_minishell()->executed == 1)
 			get_minishell()->executed = 0;
 		else
-		ft_putstr("\033[32mminishell\033[0m$ ");	
-	} else if (signbr == SIGQUIT)
+			ft_putstr("\033[32mminishell\033[0m$ ");	
+	} 
+	else if (signbr == SIGQUIT)
 	{
 		if (get_minishell()->executed == 1)
 		{
@@ -103,6 +141,8 @@ void		signal_handler(int signbr)
 int			main(void)
 {
 	int			done;
+	// int x = 0;
+	int			nbr_arg;
 	t_minishell	*minishell;
 
 	// init_minishell();
@@ -112,13 +152,25 @@ int			main(void)
 	done = 1;
 	while (done == 1)
 	{
-		ft_putstr("\033[32mminishell\033[0m$ ");
-		done = get_next_line(1, &(minishell->line));
-		// ft_printf("%d", done);
-		// if (done != 1)
-			// break;
-		// ft_printf("%s\n", line);
-		done = ft_parse_line(minishell->line);
-		ft_printf("%d", done);
+		ft_putstr("\n\033[32mminishell\033[0m$ ");
+		if (!get_next_line(1, &(minishell->line)))
+			break;
+		minishell->line = ft_strtrim(minishell->line, " \t\n\v\f\r");
+		ft_printf("line : %s\n", minishell->line);
+		
+		if (ft_strncmp(minishell->line, "\0", 1))
+		{
+			nbr_arg = ft_arg_count(minishell->line);
+			printf("nbr_arg : %d\n", nbr_arg);
+			if (!(minishell->tokens = malloc(sizeof(char *) * nbr_arg)))
+				return (0);
+			// ft_arg_split(minishell, nbr_arg);
+			minishell->tokens = ft_split(minishell->line, ' ');
+
+			// while (minishell->tokens[x])
+			done = ft_parse_line(minishell->line);
+		}
+		ft_printf("done : %d\n", done);
     }
+	ft_printf("\n");
 }
