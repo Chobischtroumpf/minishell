9563ce48ef10d2894aa5f03b7791fc280a063844
nathancolin27 <nathancolin27@gmail.com>
2020-11-04 15:08:05 +0100

    Dollar almost done, need to comment functions

--- srcs/dollar.c
+++ srcs/dollar.c
@@ -6,7 +6,7 @@
 /*   By: nathan <nathan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 11:12:57 by nathan            #+#    #+#             */
-/*   Updated: 2020/11/04 12:29:55 by nathan           ###   ########.fr       */
+/*   Updated: 2020/11/04 15:07:11 by nathan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,26 @@ int		has_dollar(char *str)
 		i++;
 	}
 	return (0);
+}
+
+char	*replace_false_dollar(char *arg, int i)
+{
+	char	*tmp;
+	char	*prefix;
+	char	*suffix;
+	int		j;
+
+	prefix = ft_substr(arg, 0, i);
+	tmp = ft_substr(arg, i, ft_strlen(arg) - i);
+	j = 0;
+	while (tmp[++j])
+		if (tmp[j] == '$')
+			break ;
+	suffix = ft_substr(tmp, j, ft_strlen(tmp) - j);
+	arg = ft_strjoin_free(prefix, suffix);
+	free(tmp);
+	free(suffix);
+	return (arg);
 }
 
 char	*replace_by_env(char *arg, char *key, char *value, int i)
@@ -62,6 +82,7 @@ char	*dollar_to_env(char *arg)
 		value = ft_strdup(tmp->value);
 		while (arg[i])
 		{
+//skip ''
 			if (!ft_strncmp((arg + i), "$$", 2))
 				arg = replace_by_env(arg, "42", "TEMP_PID", i);
 			else if (!ft_strncmp((arg + i), "$?", 2))
@@ -72,6 +93,11 @@ char	*dollar_to_env(char *arg)
 					i++;
 				else
 					arg = replace_by_env(arg, key, value, i);
+			}
+			if (arg[i] == '$')
+			{
+				arg = replace_false_dollar(arg, i);
+				i--;
 			}
 			i++;
 		}
@@ -91,10 +117,8 @@ void	check_dollar(t_cmd *cmd)
 	args = cmd->argv;
 	while (args[i])
 	{
-			args[i] = dollar_to_env(args[i]);
-		
+		args[i] = dollar_to_env(args[i]);
 		i++;
 	}
-	
 	//remove_single_quote
 }
