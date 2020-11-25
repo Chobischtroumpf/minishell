3b4b39a64690d9cfc836fbf232655e0fd1cdd1be
Alessandro Dorigo <adorgo@student.s19.be>
2020-11-25 14:50:56 +0100

    fixed last exit test

--- srcs/bltin_exit.c
+++ srcs/bltin_exit.c
@@ -6,7 +6,7 @@
 /*   By: adorigo <adorigo@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 14:52:51 by adorigo           #+#    #+#             */
-/*   Updated: 2020/11/18 13:17:29 by adorigo          ###   ########.fr       */
+/*   Updated: 2020/11/25 14:50:16 by adorigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,8 @@ static int		ft_atol_check(const char *str)
 		else if (nb > (unsigned long)LONG_MIN)
 			return (0);
 		nb *= 10;
+		if (nb > LONG_MAX && (nb +str_cpy[i] - '0') < INT_MAX)
+			return (0);
 		nb += str_cpy[i] - '0';
 		i++;
 	}
